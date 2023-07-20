0d9449e912b6cc9c4672e68cd792137904f49612
HalynaSilina <halynasilina@gmail.com>
2023-07-21 01:17:45 +0300

    add env example

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ import app from "./app.js";
 
 dotenv.config();
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
