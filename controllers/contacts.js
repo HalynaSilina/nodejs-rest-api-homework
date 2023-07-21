2d3ae85d0cac77334270c527e9ff5aba76de8e88
HalynaSilina <halynasilina@gmail.com>
2023-07-21 03:27:29 +0300

    Add model and schema
    change db logic

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,28 +1,28 @@
-import contacts from "../models/contacts.js";
+import Contact from "../models/contacts.js";
 import HttpError from "../utils/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const getAllContacts = async (_, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find({}, "-createdAt -updatedAt");
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.getContactById(id);
-  if (!result) throw HttpError(404, "Not found");
+  const result = await Contact.findById(id, "-createdAt -updatedAt");
+  if (!result) throw HttpError(404);
   res.json(result);
 };
 
 const addNewContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const deleteContactById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.removeContact(id);
-  if (!result) throw HttpError(404, "Not found");
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) throw HttpError(404);
   res.status(200).json({
     message: "contact deleted",
   });
@@ -30,8 +30,15 @@ const deleteContactById = async (req, res) => {
 
 const updateContactById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.updateContact(id, req.body);
-  if (!result) throw HttpError(404, "Not found");
+  const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
+  if (!result) throw HttpError(404);
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
+  if (!result) throw HttpError(404);
   res.json(result);
 };
 
@@ -41,4 +48,5 @@ export default {
   addNewContact: ctrlWrapper(addNewContact),
   deleteContactById: ctrlWrapper(deleteContactById),
   updateContactById: ctrlWrapper(updateContactById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
